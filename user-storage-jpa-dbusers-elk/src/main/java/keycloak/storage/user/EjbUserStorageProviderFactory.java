d6ee3e109bcee270b3be369501dd42792e5bf0c3
vasil <andr_vasil@mail.ru>
2018-03-02 20:23:42 +0300

    Реализовано заполнение dataTable

---
+++ user-storage-jpa-dbusers-elk/src/main/java/keycloak/storage/user/EjbUserStorageProviderFactory.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package keycloak.storage.user;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.storage.UserStorageProviderFactory;
+
+/**
+ *
+ * @author vasil
+ */
+public class EjbUserStorageProviderFactory implements UserStorageProviderFactory<EjbUserStorageProvider> {
+
+    @Override
+    public EjbUserStorageProvider create(KeycloakSession ks, ComponentModel cm) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getId() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
